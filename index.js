d67390e2bb1f04d2726a8b574818cd57f1a344d3
imoteriannah <imoter.iannah@student.moringaschool.com>
2023-07-23 20:05:10 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function writeCards(array, eventName){
 const thankYouMessages = [];
     for(let i =0; i < array.length; i++){
 
-        thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+        thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
     }
     return thankYouMessages;
 }
